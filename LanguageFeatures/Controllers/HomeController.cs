ca879783acf37abef26322e767675d3536eb0984
D- CODE <darek_sokol@hotmail.com>
2020-09-27 23:28:18 +0200

    Working on Language Features

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View(new string[] { "C#", "Language", "Features"});
+
+            List<string> results = new List<string>();
+
+            foreach(Product p in Product.GetProducts())
+            {
+                //The static GetProducts method defined by the Product class returns an array of objects that I inspect in the Index action method to get a list of the Name and Price values. The problem is that both the object in the array and the value of the properties could be null, which means I can’t just refer to p.Name or p.Price within the foreach loop without causing a NullReferenceException.To avoid this, I used the null conditional operator.
+
+                //The null conditional operator is a single question mark (the ? character). If p is null, then name will be set to null as well. If p is not null, then name will be set to the value of the Person.Name property. The Price property is subject to the same test.Notice that the variable you assign to when using the null conditional operator must be able to be assigned null, which is why the price variable is declared as a nullable decimal(decimal ?).
+
+                //The null conditional operator ensures that I don’t get a NullReferenceException when navigating through the object properties, and the null coalescing operator ensures that I don’t include null values in the results displayed in the browser.
+
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+
+                //The null conditional operator can be applied to each part of a chain of properties.
+                string relatedName = p?.Related?.Name ?? "<None>";
+
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+            }
+            return View(results);
+        }
+    }
+}
---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        //The null conditional operator can be chained to navigate through a hierarchy of objects, which is where it becomes an effective tool for simplifying code and allowing safe navigation. I have added a property to the Product class that creates a more complex object hierarchy.
+        public Product Related { get; set; }
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+            Product lifejacket = new Product
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
+
+            //Each Product object has a Related property that can refer to another Product object. In the GetProducts method, I set the Related property for the Product object that represents a kayak.Listing shows how I can chain the null conditional operator to navigate through the object properties without causing an exception.
+            kayak.Related = lifejacket;
+
+
+        //            The result is that the relatedName variable will be null when p is null or when p.Related is null.Otherwise, the variable will
+        //            be assigned the value of the p.Related.Name property. Restart ASP.NET Core and request http://localhost:5000, and you will see
+        //            the following output in the browser window:
+
+        //          Name: Kayak, Price: 275, Related: Lifejacket
+        //          Name: Lifejacket, Price: 48.95, Related: < None >
+        //          Name: < No Name >, Price: 0, Related: < None >
+
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
